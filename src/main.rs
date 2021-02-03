4a9df2244b83c290562b3c0f3017c17eab330c35
esposm03 <36164633+esposm03@users.noreply.github.com>
2021-02-03 15:59:11 +0100

    Hello world kernel!!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+static HELLO: &[u8] = b"Hello world!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buf = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buf.offset(i as isize * 2) = byte;
+            *vga_buf.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
