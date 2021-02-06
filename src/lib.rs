434ba82bacddc0f2d12e821a469a9321e7753d4f
esposm03 <36164633+esposm03@users.noreply.github.com>
2021-02-06 17:19:44 +0100

    Breakpoint interrupt handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 
+#![feature(abi_x86_interrupt)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 pub mod vga_buffer;
+pub mod interrupts;
 pub mod serial;
 
 #[cfg(test)]
 use core::panic::PanicInfo;
+
+pub fn init() {
+    interrupts::init_idt();
+}
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -35,6 +41,7 @@ pub fn test_runner(tests: &[&dyn Testable]) {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use rpi_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello world!");
 
+    rpi_os::init();
+
     #[cfg(test)]
     test_main();
 
